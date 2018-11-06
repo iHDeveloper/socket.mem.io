ba13a4030fd6e9c19ca044dcea3e6aad16346952
iHDeveloper <hamzadevelop@gmail.com>
2018-11-06 20:11:02 +0300

    Add Basics of Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-import { SHA256 } from "crypto-js";
+import * as SocketIO from "socket.io";
+
+export class Server {
+    public readonly options: ServerOptions;
+    private server: SocketIO.Server;
+
+    constructor(options?: ServerOptions) {
+        if (options === undefined) {
+            this.options = new ServerOptions();
+        } else {
+            this.options = options;
+        }
+        this.server = SocketIO.default(this.options);
+    }
+
+    public listen(port?: number): void {
+        if (port === undefined) {
+            port = this.options.port;
+        } else {
+            this.options.port = port;
+        }
+        this.server.listen(port, this.options);
+    }
+}
+
+export class ServerOptions implements SocketIO.ServerOptions {
+    public port: number = 8000;
+    public path: string = "/socket.io";
+    public serveClient: boolean = true;
+    public adapter: SocketIO.Adapter | undefined;
+    public origins: string|string[] = "*:*";
+    public pingTimeout: number = 60 * 1000;
+    public pingInterval: number = 25 * 1000;
+    public maxHttpBufferSize: number = 10E7;
+    public transports: string[] = ["polling", "websocket"];
+    public allowUpgrades: boolean = true;
+    public perMessageDeflate: object | boolean = true;
+    public httpCompression: object | boolean = true;
+    public cookie: string | boolean = "io";
+}
