ede82fc47960100fefdb56a954e46ad89ea1ff54
iHDeveloper <hamzadevelop@gmail.com>
2018-11-15 20:23:33 +0300

    Add indexof and static functions for full control for untils

--- src/until.ts
+++ src/until.ts
@@ -1,12 +1,25 @@
 export class Until {
 
-    public has(array: string[], toFind: string) {
-        for (const i of array) {
-            if (i === toFind) {
-                return true;
+    public static hasInArray(array: string[], toFind: string) {
+        const index: undefined | number = Until.indexInArray(array, toFind);
+        if (index === undefined) {
+            return false;
+        }
+        return true;
+    }
+
+    public static indexInArray(array: string[], toFind: string): undefined | number {
+        for (let i: number = 0; i < array.length; i++) {
+            const element: string = array[i];
+            if (element === toFind) {
+                return i;
             }
         }
-        return false;
+        return undefined;
+    }
+
+    public has(array: string[], toFind: string) {
+        return Until.hasInArray(array, toFind);
     }
 
 }
